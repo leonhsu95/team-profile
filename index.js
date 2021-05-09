750f36e66c8d22fe3e52d349ab76c45850b426f7
Leon Hsu <leonhsu95@gmail.com>
2021-05-09 11:18:06 +1000

    Questions added

--- index.js
+++ index.js
@@ -4,24 +4,6 @@ const fs = require('fs');
 const util = require('util');
 const writeFilePromise = util.promisify(fs.writeFile);
 
+const {managerQuestions,engineerQuestions,internQuestions,addMember} = require("./questions");
 
-// Validation
-let inputVal = (input) => {
-   return (!input || input.trim() === "" ? "This field is required. Please try again" : true);
-};
-
-let emailRegex = (input) => {
-   return (input.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g) ? true : "Please enter a valid email address");
-};
-
-// Create an array of questions for user input
-const questions = [
-   {
-      type: "input",
-      name: "userName",
-      message: "What is your GitHub username?", 
-      validate: inputVal
-   },    
-   
-];
-
+inquirer.prompt(managerQuestions)
---
+++ questions.js
@@ -0,0 +1,139 @@
+// Validation
+let inputVal = (input) => {
+    return (!input || input.trim() === "" || input.length < 1 ? "This field is required. Try again." : true);
+};
+
+let numberVal = (input) => {
+    return (isNaN(input) ? "Invalid ID Number. Try again." : true);
+};
+
+let emailVal = (input) => {
+    return (input.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g) ? true : "Please enter a valid email address");
+};
+
+// function idVal(input){
+//    if(managerQuestions.id == teamQuestions.id){
+//       return `Employee ID can not match existing manager ID. Try again.`;
+//    }
+//    if(!isNaN(input)){
+//       return `Invalid ID Number. Try again.`
+//    }
+//    else{
+//       return true;
+//    }
+// }
+
+
+// Create an array of questions for team
+const managerQuestions = [
+{
+    type: "input",
+    name: "company",
+    message: "Please enter your company name.", 
+    validate: inputVal
+},
+{
+    type: "input",
+    name: "id",
+    default: "0001",
+    message: "Please enter manager's employee ID.", 
+    validate: numberVal
+}, 
+{
+    type: "input",
+    name: "name",
+    message: "Please enter manager's name.", 
+    validate: inputVal
+}, 
+{
+    type: "input",
+    name: "phone",
+    message: "Please enter manager's office number.", 
+    validate: numberVal
+},
+{
+    type: "input",
+    name: "email",
+    message: "Please enter manager's email address.", 
+    validate: emailVal
+},      
+]   
+
+const addMember = [
+{
+    type: "list",
+    name: "role",
+    message: "Adding more members to your team?",
+    choices: ["YES", "NO"]
+}, 
+{
+    type: "list",
+    name: "role",
+    message: "What type of employee are you adding?",
+    choices: ["Engineer", "Intern", "Manager"]
+}     
+];
+
+const engineerQuestions = [
+{
+    type: "input",
+    name: "id",
+    default: "0002",
+    message: "Please enter employee ID.", 
+    validate: numberVal
+}, 
+{
+    type: "input",
+    name: "name",
+    message: "Please enter employee's name.", 
+    validate: inputVal
+}, 
+{
+    type: "input",
+    name: "github",
+    message: "Please enter employee's GitHub username.", 
+    validate: inputVal
+},
+{
+    type: "input",
+    name: "email",
+    message: "Please enter employee's email address.", 
+    validate: emailVal
+},      
+];
+
+const internQuestions = [
+{
+    type: "input",
+    name: "id",
+    default: "0002",
+    message: "Please enter employee ID.", 
+    validate: numberVal
+}, 
+{
+    type: "input",
+    name: "name",
+    message: "Please enter employee's name.", 
+    validate: inputVal
+}, 
+{
+    type: "input",
+    name: "school",
+    message: "Please enter employee's school.", 
+    validate: inputVal
+},
+{
+    type: "input",
+    name: "email",
+    message: "Please enter manager's email address.", 
+    validate: emailVal
+},      
+];
+
+module.exports = {
+    managerQuestions,
+    engineerQuestions,
+    internQuestions,
+    addMember
+}
+
